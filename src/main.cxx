53a1771702a9e3fb30bc438dd3ed9bbc5fcd1785
zxvnme <zxvnme@protonmail.com>
2019-08-09 13:05:54 +0200

    Implement TOML configuration and file blacklist

--- src/main.cxx
+++ src/main.cxx
@@ -4,8 +4,17 @@
 #include <fstream>
 
 #include "error.hxx"
+#include "../toml/toml.hpp"
 
 namespace fs = std::filesystem;
+
+bool passBlacklistedEntries(const fs::directory_entry& entry, const std::vector<std::string_view>& blacklist) {
+    for (const auto &blacklisted : blacklist) {
+        if (entry.path().filename().string() == blacklisted.data())
+            return false;
+    }
+    return true;
+}
 
 int main() {
     std::string output_path;
@@ -25,22 +34,24 @@ int main() {
     if (!fs::is_directory(input))
         return errorHandler.error(ERROR_CODE::ISNT_DIRECTORY);
 
-    for (const auto &entry : fs::directory_iterator(input)) {
-        if (entry.path().filename() == "output.txt")
-            continue;
+    const auto config = toml::parse(input + "rsconfig.toml");
+    const auto blacklist = toml::find<std::vector<std::string_view>>(config, "blacklist");
 
-        if (entry.is_directory()) {
-            files.emplace_back("+-- " + entry.path().filename().string().append("/"));
-            for (const auto &sub_entry : fs::directory_iterator(entry.path())) {
-                if (sub_entry.is_directory()) {
-                    files.emplace_back("   +---- " + sub_entry.path().filename().string().append("/"));
-                    continue;
+    for (const auto &entry : fs::directory_iterator(input)) {
+        if (passBlacklistedEntries(entry, blacklist)) {
+            if (entry.is_directory()) {
+                files.emplace_back("+-- " + entry.path().filename().string().append("/"));
+                for (const auto &sub_entry : fs::directory_iterator(entry.path())) {
+                    if (sub_entry.is_directory()) {
+                        files.emplace_back("   +---- " + sub_entry.path().filename().string().append("/"));
+                        continue;
+                    }
+                    files.emplace_back("   +---- " + sub_entry.path().filename().string());
                 }
-                files.emplace_back("   +---- " + sub_entry.path().filename().string());
+                continue;
             }
-            continue;
+            files.emplace_back("+-- " + entry.path().filename().string());
         }
-        files.emplace_back("+-- " + entry.path().filename().string());
     }
 
     for (const auto &entry : files)
